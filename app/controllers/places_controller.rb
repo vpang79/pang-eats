947ab357287b58de88b24b719704adf28706fca5
Vincent Pang <vpang23@gmail.com>
2019-08-29 04:27:17 +0000

    added edit form for place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,6 +18,11 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+
   private
 
   def place_params

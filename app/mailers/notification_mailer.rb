cd3ed6f55e59e721ab1d7f2999d0bc9dee5584f2
Vincent Pang <vpang23@gmail.com>
2019-09-12 04:01:46 +0000

    email notification works!

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,11 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@pangsterapp.com"
 
-  def comment_added
-    mail(to: "vpang23@gmail.com",
-      subject: "A comment has been added to your place - test")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+      subject: "A comment has been added to #{@place.name} - test")
   end
 
 
